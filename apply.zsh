5a40b79d1d8b3465d0ada3a4ac0a9d52c1adce7d
TakeshiAkehi <takeshi.akehi@terra-drone.co.jp>
2022-10-04 17:59:43 +0900

    addranger

--- apply.zsh
+++ apply.zsh
@@ -3,7 +3,13 @@ CDIR=$(cd $(dirname $0);pwd)
 
 ln -s -f ${CDIR}/dotfiles/.p10k.zsh ${HOME}/.p10k.zsh 
 ln -s -f ${CDIR}/dotfiles/.vimrc ${HOME}/.vimrc
-ln -s -f ${CDIR}/dotfiles/verbs.hjson ${HOME}/.config/broot/verbs.hjson
+# ln -s -f ${CDIR}/dotfiles/verbs.hjson ${HOME}/.config/broot/verbs.hjson
+
+ln -s -f ${CDIR}/dotfiles/commands_full.py ${HOME}/.config/ranger/commands_full.py
+ln -s -f ${CDIR}/dotfiles/commands.py ${HOME}/.config/ranger/commands.py
+ln -s -f ${CDIR}/dotfiles/rc.conf ${HOME}/.config/ranger/rc.conf
+ln -s -f ${CDIR}/dotfiles/rfile.conf ${HOME}/.config/ranger/rfile.conf
+ln -s -f ${CDIR}/dotfiles/scope.sh ${HOME}/.config/ranger/scope.sh
 
 mkdir ~/dotrc -p
 
@@ -18,6 +24,9 @@ echo "source ${HOME}/dotrc/.p10k_conf" >> ~/.zshrc
 
 ln -s -f ${CDIR}/configs/.fzf_widgets_conf ${HOME}/dotrc/.fzf_widgets_conf
 echo "source ${HOME}/dotrc/.fzf_widgets_conf" >> ~/.zshrc
+
+ln -s -f ${CDIR}/configs/.ranger_conf ${HOME}/dotrc/.ranger_conf
+echo "source ${HOME}/dotrc/.ranger_conf" >> ~/.zshrc
 
 source ~/.zshrc
 source ~/.zshrc
--- init.zsh
+++ init.zsh
@@ -6,12 +6,15 @@ touch ~/.zshrc
 apt update
 apt install -y git curl wget fzf fzy
 
-apt install -y gcc g++ make cmake
-wget "https://dystroy.org/broot/download/x86_64-linux/broot"
-mv broot /usr/local/bin
-chmod +x /usr/local/bin/broot
-broot
-source ~/.zshrc
+# apt install -y gcc g++ make cmake
+# wget "https://dystroy.org/broot/download/x86_64-linux/broot"
+# mv broot /usr/local/bin
+# chmod +x /usr/local/bin/broot
+# broot
+# source ~/.zshrc
+
+apt-get install -y ranger w3m lynx highlight atool mediainfo caca-utils
+ranger --copy-config=all
 
 bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 source ~/.zshrc
