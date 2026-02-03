b6d503d08774b648eee41609ebc9b3ce10d1a619
TakeshiAkehi <takeshi.akehi@terra-drone.co.jp>
2026-02-03 14:19:49 +0900

    delete fzf commands

--- dotrc.zsh
+++ dotrc.zsh
@@ -5,7 +5,6 @@ WDIR=${CDIR}/addon
 
 source ${WDIR}/zsh_conf.zsh
 source ${WDIR}/zinit_conf.zsh
-source ${WDIR}/fzf_commands.zsh
 source ${WDIR}/tui.zsh
 # source ${WDIR}/gnu_commands.zsh
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
